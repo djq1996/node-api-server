e13f138dde4913ce33af3fd7a5b8eec32d9fa0b2
aliyun_djq <47.100.26.246>
2018-11-05 20:16:50 +0800

    update

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const router = require('./router');
 const app = new Koa();
 app.use(router.routes());
-const port = 5001;
+const port = 3000;
 app.listen(port, () => {
   console.log(`server is starting at port ${port}`);
 });
