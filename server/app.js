ddb2c1f2be9be80afabde85df1bdc3ff55c1674f
dongjianqiang <dongjianqiang@ann9.com>
2018-11-06 11:16:57 +0800

    update

--- server/app.js
+++ server/app.js
@@ -2,8 +2,8 @@ const Koa = require('koa');
 const router = require('./router');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
-app.use(router.routes());
 app.use(bodyParser());
+app.use(router.routes());
 const port = 3000;
 app.listen(port, () => {
   console.log(`server is starting at port ${port}`);
